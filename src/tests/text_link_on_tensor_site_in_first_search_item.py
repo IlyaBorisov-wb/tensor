8b2e409432015f5263c33dcb14916744f344ec68
IlyaBorisov-wb <augusrbrouns@gmail.com>
2023-09-01 13:22:59 +0300

    add decorator, improve locator name

--- src/tests/text_link_on_tensor_site_in_first_search_item.py
+++ src/tests/text_link_on_tensor_site_in_first_search_item.py
@@ -1,11 +1,14 @@
 from src.pages.yeandex_serach_page import YandexSearchPage
 from src.locators.yandex_search import YandexSearch
+from src.utils.add_driver import pass_driver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 
-def test_search_link_on_company_web_page():
 
-    page = YandexSearchPage()
+@pass_driver
+def test_search_link_on_company_web_page(driver=None):
+
+    page = YandexSearchPage(driver)
 
     page.open_search_page()
     sleep(1)
@@ -17,7 +20,7 @@ def test_search_link_on_company_web_page():
 
     assert page.is_display_suggest_table() == True, 'No display suggest table'
 
-    page.send_keys_to_element(YandexSearch.SEARCH_INPUT, Keys.ENTER)
+    page.send_keys_to_element(YandexSearch.SEARCH_INPUT_ON_YA_RU_PAGE, Keys.ENTER)
     sleep(1)
     page.close_set_yandex_default_browser()
     sleep(1)
